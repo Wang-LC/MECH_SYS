6595de4afe0fcd1a51f9dbf262f2e2fd1e3cb283
wanglc <wlc.9424@gmail.com>
2019-02-22 10:33:06 -0800

    robust to bad arguments
    add try function

--- integrate.py
+++ integrate.py
@@ -8,8 +8,17 @@ def func(x):
     return x**2+x+1
 
 
-def integrate(f, j, k, integral=100):
-    a = min()
+def integrate(f, j, k, integral=100):  # Q1
+    try:
+        f(1)
+    except TypeError:
+        raise TypeError('enter a def function')
+    try:
+        a = min([float(j), float(k)])
+        b = max([float(j), float(k)])
+    except TypeError:
+        raise TypeError('please enter number')
+
     delta = numpy.linspace(a, b, integral)
     s = 0
     for x in range(integral-1):
@@ -17,17 +26,31 @@ def integrate(f, j, k, integral=100):
     return s
 
 
-def integrate_mc(f, a, b, c, d, sample=10000):
-    n = 0
+def integrate_mc(f, a, b, T, sample=100000):  # Q2
+    try:
+        f(1)
+    except TypeError:
+        raise TypeError('enter a def function')
+    try:
+        a = float(a)
+        b = float(b)
+    except TypeError:
+        raise TypeError('please enter number')
+    try:
+        c = min([T[0], T[1]])
+        d = max([T[0], T[1]])
+    except TypeError:
+        raise TypeError('please enter a tuple')
+    count = 0
     for i in range(sample):
         x = random.uniform(a, b)
         y = random.uniform(c, d)
         if abs(y) <= abs(f(x)):
             if 0 <= y <= f(x):
-                n += 1
+                count += 1
             if 0 >= y >= f(x):
-                n -= 1
-    area = n / sample * (b - a) * (d - c)
+                count -= 1
+    area = count / sample * (b - a) * (d - c)
     if c > 0 and d > 0:
         return area + (b-a)*c
     elif c < 0 and d < 0:
@@ -36,27 +59,26 @@ def integrate_mc(f, a, b, c, d, sample=10000):
         return area
 
 
-def error_plot():
+def error_plot():  # Q3 plot the graph
     int_error = []
     mc_error = []
     sample_num = []
-    for i in range(6):
-        rei = abs(integrate(func, 10, 20, int(pow(10, i))) - 2493.333)
-        mc = abs(integrate_mc(func, 10, 20, 100, 450, int(pow(10, i)))-2493.333)
+    for i in numpy.linspace(10, 100000, 30):
+        rei = abs(integrate(func, 10, 20, int(i)) - 2493.333)
+        mc = abs(integrate_mc(func, 10, 20, (0, 450), int(i))-2493.333)
         int_error.append(rei)
         mc_error.append(mc)
-        sample_num.append(pow(10,i))
-    x = [str(i) for i in sample_num]
-    plt.plot(x, int_error, '*-', label='Reimann error')
-    plt.plot(x, mc_error, 'x-', label='MC error')
+        sample_num.append(int(i))
+    plt.plot(sample_num, int_error, '*-', label='Reimann error')
+    plt.plot(sample_num, mc_error, 'x-', label='MC error')
     plt.legend()
     plt.xlabel('number of intervals or samples')
     plt.ylabel('Absolute Error')
-    plt.title('ERROR')
+    plt.title('the convergence behavior of the two functions')
     plt.show()
 
 
-def approximate_pi(n):
+def approximate_pi(n):  # Q4
     return 4*integrate_mc(lambda x: math.sqrt(1-x**2), 0, 1, 0, 1, n)
 
 
