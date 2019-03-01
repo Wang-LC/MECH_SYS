c5c9ec81e13ea5932869fb4c69529cb2640180db
wanglc <wlc.9424@gmail.com>
2019-03-01 10:18:52 -0800

    addition

---
+++ directory1.py
@@ -0,0 +1,25 @@
+import urllib.request
+import sys
+from bs4 import BeautifulSoup
+
+
+def main():
+    input_query = sys.argv[1:]
+    query_string = ("+".join(input_query))    
+    # http://directory.oregonstate.edu/?type=search&cn=Edward+Fesser
+    web_content = urllib.request.urlopen("http://directory.oregonstate.edu/?type=search&cn=%s" % query_string)
+    html = web_content.read().decode("UTF-8")
+
+    soup = BeautifulSoup(html, "lxml").find(id = "records")
+    titleList = [tag.text for tag in soup.findAll("dt")]
+    infoList = [tag.text for tag in soup.findAll("dd")]
+   
+    #print(soup)
+    for i in range(len(titleList)):
+        print("%s: %s" % (titleList[i], infoList[i]))
+
+    # print(titleList)
+    # print(infoList)
+
+if __name__ == "__main__":
+    main()
