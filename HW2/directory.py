47ed70fc09a9ebd26939ee4a070f4d822f8ee18e
wanglc <wlc.9424@gmail.com>
2019-03-05 15:36:17 -0800

    archive hw2

--- HW2/directory.py
+++ HW2/directory.py
@@ -1,14 +1,13 @@
 import urllib.request
 import sys
 from bs4 import BeautifulSoup
-# install BeautifulSoup
 # install lxml
 
 
 base = "http://directory.oregonstate.edu/"
 
 
-class people:
+class people:  # storing four info
     def __init__(self, name, title, department, phone):
         self.name = name
         self.title = title
@@ -22,26 +21,27 @@ class people:
         # Department: Provost / Exec Vice Pres
         # Phone: 123-123-4321
         # ---------------------------------------
-        print("Name: %s\nTitle: %s\nDepartment: %s\nPhone: %s\n" % (self.name, self.title, self.department, self.phone))
+        print("Name: %s\nTitle: %s\nDepartment: %s\nPhone: %s" % (self.name, self.title, self.department, self.phone))
 
 
-def main():  # before main() raise ValueError('Please enter a name')
-    input_name = sys.argv[1:]
-    # input_name = ["Edward+Feser"]
-    name_string = ("+".join(input_name))
+def search(name):  # before main() try Command-line arguments work
+    # name = sys.argv[1:]
+    # name = ['Edward', 'Feser']
+    name_string = ("+".join(name))
+    # name_string = "Edward+Feser"
+
     # http://directory.oregonstate.edu/?type=search&cn=Edward+Fesser
-    
     url = base + "?type=search&cn=%s" % name_string
     soup = readSoup(url).find(id="records")
+
     # deal with too many returns shown on the website
     if soup is None:
         soup = readSoup(url).find("body")
-        print(soup.find('h2').text)
+        print(soup.find("h2").text)
         exit()
 
     isSingle = 1
-    # is multiple people
-    if soup.find("h2") is not None:
+    if soup.find("h2") is not None:  # is multiple people
         isSingle = 0
 
     if isSingle == 1:
@@ -51,10 +51,10 @@ def main():  # before main() raise ValueError('Please enter a name')
         results = findMultiple(soup)
         for result in results:
             result.pprint()
-            print("-" * 30)
+            print("-" * 50)
 
 
-def readSoup(url):  # read html part by id readSoup().find()
+def readSoup(url):  # find html part by id readSoup(url).find()
     web_content = urllib.request.urlopen(url)
     html = web_content.read().decode("UTF-8")
     return BeautifulSoup(html, "lxml")
@@ -85,43 +85,19 @@ def findSingle(soup):  # find person info
 
 
 def findMultiple(soup):  # deal with multiple person and no this name
-    records = soup.find_all('a', href=True, attrs={'class': ''})
+    records = soup.find_all('a', href=True, attrs={"class": ""})
     results = []
     if not records:  # no this name
-        print(soup.find('h2').text)
+        print(soup.find("h2").text)
         exit()
     else:
         for record in records:
-            results.append(findSingle(readSoup(base + record['href'])))
+            results.append(findSingle(readSoup(base + record["href"])))
         return results
-
-
-def mime_count():
-    url = "https://mime.oregonstate.edu/people"
-    content = readContent(url)
-    # set job dictionary
-    jobs = {}
-    for row in content.tbody.findAll('tr'):
-        job_title = row.findAll('td')[2].text.split('\n')
-        for item in job_title:
-            if item not in jobs:  # set a new job key
-                jobs[item] = 0
-            jobs[item] += 1
-    del jobs[""]
-    for key, value in jobs.items():
-        print("%s: %s" % (key, value))
-
-
-def readContent(url):
-    web_content = urllib.request.urlopen(url)
-    html = web_content.read().decode("UTF-8")
-    soup = BeautifulSoup(html, "lxml")
-    content = soup.findAll("table", attrs={'class': 'views-table'})[0]
-    return content
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         raise ValueError('Please enter a name')
     else:
-        main()
+        search(sys.argv[1:])
--- HW2/mime_count.py
+++ HW2/mime_count.py
@@ -1,6 +1,4 @@
 # https://mime.oregonstate.edu/people
-# -*- coding: utf-8 -*-
-# ！/usr/bin/env python
 import urllib
 from bs4 import BeautifulSoup
 
@@ -9,19 +7,15 @@ def mime_count():
     url = "https://mime.oregonstate.edu/people"
     content = readContent(url)
     jobs = {}
-    for row in content.tbody.findAll('tr'):
+    for row in content.tbody.find_all('tr'):  # read each people job
         job_title = row.findAll('td')[2].text.split('\n')
-        for item in job_title:
+        for item in job_title:  # count number
             if item not in jobs:
                 jobs[item] = 0
             jobs[item] += 1
+    # delete unused value
     del jobs[""]
     print_func(jobs)
-
-
-def print_func(jobs):
-    for key, value in jobs.items():
-        print("%s: %s" % (key, value))
 
 
 def readContent(url):
@@ -30,6 +24,14 @@ def readContent(url):
     soup = BeautifulSoup(html, "lxml")
     content = soup.findAll("table", attrs={'class': 'views-table'})[0]
     return content
+
+
+def print_func(jobs):
+    # Assistant Professor: 21
+    # Associate Professor: 12
+    # ...
+    for key, value in jobs.items():
+        print("%s: %s" % (key, value))
 
 
 if __name__ == "__main__":
