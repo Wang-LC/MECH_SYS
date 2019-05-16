8699d49e3f41a653faa4a82c0728f33477c2c9c5
Wang-LC <wlc.9424@gmail.com>
2019-05-16 00:45:55 -0700

    R

--- Lab1/words.py
+++
@@ -1,7 +0,0 @@
-def letter_count(text, letter):
-    ans = text.lower().count(letter.lower())
-    return ans
-
-if __name__ == '__main__':
-    if letter_count('halLway', 'l') == 2 and letter_count('halLway', 'L') == 2:
-        print("words passed!")
--- Lab4/lab4.py
+++
@@ -1,9 +0,0 @@
-from sin_wave import sinWave
-from distribution import distribution
-from damper import damper
-from sorted import sortPlot
-
-sinWave()
-distribution()
-damper()
-sortPlot()
